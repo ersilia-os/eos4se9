4de4122689ef4a719f0cd95caeec7589a1e1fe62
Carolina Caballero López <carolina.cl19@gmail.com>
2023-01-11 11:24:50 -0500

    model loaded

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -1,10 +1,9 @@
 # imports
 import os
 import csv
-import joblib
 import sys
-from rdkit import Chem
-from rdkit.Chem.Descriptors import MolWt
+from stout import translate_forward
+import time
 
 # parse arguments
 input_file = sys.argv[1]
@@ -13,28 +12,24 @@ output_file = sys.argv[2]
 # current file directory
 root = os.path.dirname(os.path.abspath(__file__))
 
-# checkpoints directory
-checkpoints_dir = os.path.abspath(os.path.join(root, "..", "..", "checkpoints"))
+start = time.time()
+def my_model(smiles_list):
+    return {smi:translate_forward(smi) for smi in smiles_list}
 
-# read checkpoints (here, simply an integer number: 42)
-ckpt = joblib.load(os.path.join(checkpoints_dir, "checkpoints.joblib"))
-
-# model to be run (here, calculate the Molecular Weight and add ckpt (42) to it)
-def my_model(smiles_list, ckpt):
-    return [MolWt(Chem.MolFromSmiles(smi))+ckpt for smi in smiles_list]
-    
 # read SMILES from .csv file, assuming one column with header
 with open(input_file, "r") as f:
     reader = csv.reader(f)
     next(reader) # skip header
     smiles_list = [r[0] for r in reader]
-    
+
 # run model
-outputs = my_model(smiles_list, ckpt)
+iupacs_names = my_model(smiles_list)
 
 # write output in a .csv file
 with open(output_file, "w") as f:
     writer = csv.writer(f)
-    writer.writerow(["value"]) # header
-    for o in outputs:
-        writer.writerow([o])
+    writer.writerow(["smiles", "iupacs_names"]) # header
+    for i, o in iupacs_names.items():
+        writer.writerow([i, o])
+
+print("Time taken for per prediction is {} sec\n".format((time.time() - start) / 100))
