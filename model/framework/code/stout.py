a39b5366295b15add840efe28d45b83b2c8af49d
Carolina Caballero López <carolina.cl19@gmail.com>
2023-01-11 18:02:33 -0500

    Update stout.py

--- model/framework/code/stout.py
+++ model/framework/code/stout.py
@@ -20,8 +20,10 @@ gpus = tf.config.experimental.list_physical_devices("GPU")
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
 
-#Modified Carolina-carcablop
+#Modified by Carolina-carcablop
 # current file directory
+#the models are downloaded in the checkpoint folder,
+#so I set the PYSTOM_HOME variable to the path where the models are located.
 root = os.path.dirname(os.path.abspath(__file__))
 pystow_home = os.path.abspath(os.path.join(root, "..",".."))
 
@@ -30,26 +32,9 @@ os.environ['PYSTOW_HOME']= pystow_home
 # checkpoints directory
 default_path= pystow.join("checkpoints")
 
-#These lines are commented out because the models are downloaded in the checkpoint folder,
-#so I set the PYSTOM_HOME variable to the path where the models are located.
-'''
-# Set path
-default_path = pystow.join("STOUT-V2", "models")
-
-# model download location
-model_url = "https://storage.googleapis.com/decimer_weights/models.zip"
-model_path = str(default_path) + "/translator_forward/"
-
-# download models to a default location
-if not os.path.exists(model_path):
-    helper.download_trained_weights(model_url, default_path)'''
-
 
 # Load the packed model forward
 reloaded_forward = tf.saved_model.load(default_path.as_posix()+"/translator_forward")
-
-# Load the packed model forward
-#reloaded_reverse = tf.saved_model.load(default_path.as_posix()+"/translator_reverse")
 
 
 def translate_forward(smiles: str) -> str:
@@ -82,30 +67,3 @@ def translate_forward(smiles: str) -> str:
         return result
     else:
         return "Could not generate IUPAC name from invalid SMILES."
-
-
-'''def translate_reverse(iupacname: str) -> str:
-    """Takes user input splits them into words and generates tokens.
-    Tokens are then passed to the model and the model predicted tokens are retrieved.
-    The predicted tokens gets detokenized and the final result is returned in a string format.
-
-    Args:
-        iupacname (str): user input IUPAC names in string format.
-
-    Returns:
-        result (str): The predicted SMILES in string format.
-    """
-
-    # Load important pickle files which consists the tokenizers and the maxlength setting
-    targ_lang = pickle.load(open(default_path.as_posix()+"/assets/tokenizer_input.pkl", "rb"))
-    inp_lang = pickle.load(open(default_path.as_posix()+"/assets/tokenizer_target.pkl", "rb"))
-    inp_max_length = pickle.load(open(default_path.as_posix()+"/assets/max_length_targ.pkl", "rb"))
-
-    splitted_list = list(iupacname)
-    tokenized_IUPACname = " ".join(map(str, splitted_list))
-    decoded = helper.tokenize_input(tokenized_IUPACname, inp_lang, inp_max_length)
-
-    result_predited = reloaded_reverse(decoded)
-    result = helper.detokenize_output(result_predited, targ_lang)
-
-    return result'''
