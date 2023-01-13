6e4a5825b33962c14f7b16fe64e8957ed949ecb9
Carolina Caballero López <carolina.cl19@gmail.com>
2023-01-13 18:09:24 -0500

    Update main.py

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -12,9 +12,8 @@ output_file = sys.argv[2]
 # current file directory
 root = os.path.dirname(os.path.abspath(__file__))
 
-start = time.time()
 def my_model(smiles_list):
-    return {smi:translate_forward(smi) for smi in smiles_list}
+    return [translate_forward(smi) for smi in smiles_list]
 
 # read SMILES from .csv file, assuming one column with header
 with open(input_file, "r") as f:
@@ -28,8 +27,6 @@ iupacs_names = my_model(smiles_list)
 # write output in a .csv file
 with open(output_file, "w") as f:
     writer = csv.writer(f)
-    writer.writerow(["smiles", "iupacs_names"]) # header
-    for i, o in iupacs_names.items():
-        writer.writerow([i, o])
-
-print("Time taken for per prediction is {} sec\n".format((time.time() - start) / 100))
+    writer.writerow(["iupacs_names"]) # header
+    for o in iupacs_names:
+        writer.writerow([o])
